987ebe1289cd09c284b33987eec36e6a2ec7a5e0
raphael oliveira <raphael.oliveira@cortex-intelligence.com>
2024-09-07 15:17:41 -0300

    layout

---
+++ src/components/LayoutComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+
+<template>
+    <main class="page">
+        <nav class="menu">
+            <RouterLink to="/services">Serviços</RouterLink>
+            <RouterLink to="/">Sair</RouterLink>
+        </nav>
+        <div class="page-content">
+            <slot></slot>
+        </div>
+    </main>
+</template>
+
+
+<style scoped>
+.page {
+    display: flex;
+}
+
+.menu {
+    width: 20%;
+    height: 100vh;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.page-content {
+    background-color: white;
+    width: 100%;
+}
+</style>
--- src/views/ServicesPage.vue
+++ src/views/ServicesPage.vue
@@ -1,8 +1,13 @@
+<script setup lang="ts">
+import LayoutComponent from '@/components/LayoutComponent.vue';
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <LayoutComponent>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+  </LayoutComponent>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>

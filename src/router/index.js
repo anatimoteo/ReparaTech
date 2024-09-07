58988bfa132fc10376b378474f1795c8d03affb1
anatimoteo <analu2807.info@gmail.com>
2024-09-07 14:52:33 -0300

    rotas service

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginPage from '../views/LoginPage.vue'
+import ServicesPage from '../views/ServicesPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,13 +10,20 @@ const router = createRouter({
       name: 'login',
       component: LoginPage
     },
+
+    {
+      path: '/services',
+      name: 'services',
+      component: ServicesPage
+    },
+
     {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/ServicesPage.vue')
     }
   ]
 })
